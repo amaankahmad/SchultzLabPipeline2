3d8c853ae503d0cd0a8f24355e6ae9d6b6fbfe49
ryanling777 <ryannicling@gmail.com>
2022-03-15 13:42:34 +0000

    Integrated Backend with React

--- pipeline/api/views.py
+++ pipeline/api/views.py
@@ -1,8 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 # Create your views here.
 # Where all our endpoints go
 
 #takes in a request returns a response
-def main(request):
-    return HttpResponse("Hello")
+#queryset stores all the room objects currently in the database.
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
